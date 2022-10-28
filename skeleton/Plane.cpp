2d0dc2cbe74a0df81627ae50011e01e35b3099e9
Usuario Local <pablarre@ucm.es>
2022-10-28 10:33:49 +0200

    Mejores Colores

--- skeleton/Plane.cpp
+++ skeleton/Plane.cpp
@@ -3,7 +3,7 @@
 Plane::Plane(Vector3 Pos)
 {
 	pos = physx::PxTransform(Pos.x, Pos.y, Pos.z);
-	renderItem = new RenderItem(CreateShape(physx::PxBoxGeometry(1000, 0.1, 1000)), &pos, { 0.1, 0.1, 0.3, 1 });
+	renderItem = new RenderItem(CreateShape(physx::PxBoxGeometry(1000, 0.1, 1000)), &pos, { 0.05, 0.05, 0.1, 1 });
 }
 
 Plane::~Plane()
